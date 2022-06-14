8d8f09663dc32231004da9a555b2e2491a4d3137
atharva2601 <atharvap987@gmail.com>
2022-06-14 15:41:35 +0530

    task

--- todo/src/components/home.js
+++ todo/src/components/home.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
 
@@ -7,29 +7,72 @@ const Home = () => {
     
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const nav = useNavigate();
-        const handleEmailChange = (mail) => {
-            setEmail(mail.target.value);
-        };
-        
-        const handlePasswordChange = (mail) => {
-            setPassword(mail.target.value);
-        };
+    const [creatingAccount, setCreatingAccount] = useState(false);
+    const [accountInfo, setAccountInfo] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
 
-        const handleSignIn = () => {
-            signInWithEmailAndPassword(auth, email, password).then(() => {
-                nav('/main')
-            }).catch((err) => alert(err.message));
-        };
+    const nav = useNavigate();
+
+    useEffect (() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                nav('/main');
+            }
+        });
+    }, []);
+
+    const handleEmailChange = (mail) => {
+        setEmail(mail.target.value);
+    };
+        
+    const handlePasswordChange = (mail) => {
+        setPassword(mail.target.value);
+    };
+
+    const handleSignIn = () => {
+        signInWithEmailAndPassword(auth, email, password).then(() => {
+            nav('/main')
+        }).catch((err) => alert(err.message));
+    };
+
+    const handleCreate = () => {
+        if(accountInfo.password !== accountInfo.confirmPassword) {
+            alert("Password Doesn't Match")
+            return
+        }
+        createUserWithEmailAndPassword(auth, accountInfo.email, accountInfo.password)
+        .then(() => {
+            nav("/main");
+        })
+        .catch((err) => alert(err.message));
+    };
 
     return (
         <div className='home'>
             <h1>ToDo</h1>
             <div className='auth'>
-                <input type="email" onChange={handleEmailChange} value={email} />
-                <input type="password" onChange={handlePasswordChange} value={password} />
-                <button onClick={handleSignIn} >Sign In</button>
-                <a href="">Sign Up</a>
+                {creatingAccount ?
+                (
+                    <>
+                    <input type="email" placeholder='Email' value={accountInfo.email} onChange={(mail) => setAccountInfo({...accountInfo, email: mail.target.value})} />
+                    <input type="password" placeholder='Password' value={accountInfo.password} onChange={(mail) => setAccountInfo({...accountInfo, password: mail.target.value})} />
+                    <input type="password" placeholder='Confirm Password' value={accountInfo.confirmPassword} onChange={(mail) => setAccountInfo({...accountInfo, confirmPassword: mail.target.value})} />
+                    <button onClick={handleCreate} >Sign Up</button>
+                    <button onClick={() => setCreatingAccount(false)} >Go Back</button>
+                    </>
+                ) : 
+                (   
+                    <>
+                        <input type="email" placeholder='Email' onChange={handleEmailChange} value={email} />
+                        <input type="password" placeholder='Password' onChange={handlePasswordChange} value={password} />
+                        <button onClick={handleSignIn} >Sign In</button>
+                        <button onClick={() => setCreatingAccount(true)} >Sign Up</button>
+                    </>
+                )
+                }
             </div>
         </div>
     );
--- todo/src/components/main.js
+++ todo/src/components/main.js
@@ -1,9 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { set, ref, onValue, remove, update } from "firebase/database";
+import {uid} from 'uid';
+
 
 const Main = () => {
+    const [task, setTask] = useState("");
+    const [tasks, setTasks ] = useState([]);
+    const [edit, setEdit] = useState(false);
+    const [temUidd, setTemUidd] = useState("");
+
+    const nav = useNavigate();
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if(user){
+                onValue(ref(db, `/${auth.currentUser.uid}`), snapshot => {
+                    setTasks([]);
+
+                    const data = snapshot.val();
+                    if (data !== null) {
+                        Object.values(data).map((task) => {
+                            setTasks((oldArray) => [...oldArray, task]);
+                        });
+                    }
+                })
+            }
+            else if(!user) {
+                nav("/");
+            }
+        });
+    }, []);
+
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            nav("/");
+        })
+            .catch((err) => {alert(err.message);
+            });
+    };
+
+    const write = () => {
+        const uidd = uid(); 
+        set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
+            task: task,
+            uidd: uidd,
+        });
+
+        setTask("");
+    };
+
+    const handleDelete = (uid) => {
+        remove(ref(db, `/${auth.currentUser.uid}/${uid}`));
+    };
+
+    const handleEdit = (task) => {
+        setEdit(true)
+        setTask=(task.task);
+        setTemUidd(task.uidd)
+    };
+
+    const handleEditConf = () => {
+        update(ref(db, `/${auth.currentUser.uid}/${temUidd}`), {
+            task: task,
+            temUidd: temUidd
+        });
+
+        setTask("");
+    };
+
     return (
         <div>
-            <h1>Try hai</h1>
+            <input type="text" placeholder="Add Task to Perform" value={task} onChange= {(t) => setTask(t.target.value)} />
+            {tasks.map((task) => (
+                <div>
+                    <h5>{task.task}</h5>
+                    <button onClick={() => handleEdit(task)} >Edit</button>
+                    <button onClick={() => handleDelete(task.uidd) } >Delete</button>
+                </div>
+            ))}
+            {/* <button onClick={write} >Add</button> */}
+
+            {edit ? (
+                <div>
+                    <button onClick={handleEditConf} >Confirm</button>
+                </div>
+            ) : ( 
+                <div>
+                    <button onClick={write} >Add</button>
+                </div>
+            )}
+            <button onClick={handleSignOut} >Sign Out</button>
         </div>
     );
 };
